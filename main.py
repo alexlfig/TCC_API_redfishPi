8d495f8fb9a94cd451a0d6c61e992fb0d2d17e57
alexlfig <alexsleite@outlook.com>
2023-10-01 20:05:00 -0300

    atualização chamadas dos endpoints

--- main.py
+++ main.py
@@ -1,29 +1,53 @@
 
 from flask import Flask
 import psutil
-import os
-import io
+import json
+
 
 app = Flask(__name__)
 
 @app.route('/')#decorator
 def index():
-    return'mostrá infomação'
+    return'Bem Vindo a RedfishPi'
     #return '@odata.context": "/redfish/v1/$metadata#ServiceRoot",'
 
 @app.route('/redfish/v1/', methods=['GET'])
-
-def get_systems():  
-    systemInfo = {
-        'freqCpu': psutil.cpu_freq()
+def get_v1():  
+    raizV1 = {
+        'Chassis': '<a href=/redfish/v1/Chassis>/redfish/v1/Chassis</a>'
     }
-    return '<a href="/redfish/v1/memory">Acesse o Google</a>'
-    #return systemInfo
+    v1 = json.dumps(raizV1)
+    #return '<a href="/redfish/v1/memory">Acesse o Google</a>'
+    return v1
+
+@app.route('/redfish/v1/Chassis', methods=['GET'])
+def get_Chassis():
+    #Implemente a lógica para obter informações do sistema usando Redfish aqui
+    return 'Informações de Chassis'
+
+@app.route('/redfish/v1/Systems', methods=['GET'])
+def get_Systems():
+    systemInfo = {
+        'memory': '<a href=/redfish/v1/memory>/redfish/v1/memory</a>',
+        'freqCpu': psutil.cpu_freq(),
+        'Processors': '<a href=/redfish/v1/Processors>/redfish/v1/Processors</a>',
+    }
+    v1 = json.dumps(systemInfo)
+    #return '<a href="/redfish/v1/memory">Acesse o Google</a>'
+    return v1
 
 @app.route('/redfish/v1/memory', methods=['GET'])
 def get_memory():
     #Implemente a lógica para obter informações do sistema usando Redfish aqui
     return 'Informações de memoria'
+
+@app.route('/redfish/v1/Processors', methods=['GET'])
+def get_processors():
+    processorsInfo = {
+        'Processors': psutil.cpu_count(),
+    }
+    return processorsInfo
+
 
 
 if __name__ == '__main__':
